48b75adfa420c4862ab13884c96ebb404367248e
Dmitry Kovalev <kovalev@haulmont.com>
2019-11-22 14:57:43 +0400

    added writiing to the fs

--- controllers/products.js
+++ controllers/products.js
@@ -17,13 +17,14 @@ exports.postProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/shop",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+  Product.fetchAll(products => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/shop",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
   });
 };
