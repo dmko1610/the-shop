7a5ff904a8d0ab4a64052749438415a896ab19bd
Dmitry Kovalev <trigger1610@gmail.com>
2020-05-28 17:43:59 +0400

    deleted database
    added mongoose

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const db =
+  "mongodb+srv://dmko1610:ta4Q6ut7ap3wYUv7@trigger-cluster-arnah.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
+
 const User = require("./models/user");
 
 const app = express();
@@ -32,6 +35,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(db)
+  .then(() => app.listen(3000))
+  .catch((err) => console.log(err));
